0c5c6d01c14a3bfe7d4cdeede83e44eb08f2e5b2
zky829 <nikita@Nikitas-MacBook-Pro.local>
2017-09-05 17:53:24 +0100

    Add Jasmine tests

---
+++ src/spec/server.spec.js
@@ -0,0 +1,39 @@
+const request = require('request');
+
+const base_url = "http://localhost:3000/"
+
+describe('URL shortener server', () => {
+  describe('GET /', () => {
+    it('returns status code 200', (done) => {
+      request.get(base_url, (error, response, body) => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    it('returns index page with a title, label, input field, and button', (done) => {
+      request.get(base_url, (error, response, body) => {
+        expect(body.includes('<h1>URL Shortener</h1><br>')).toBeTrue;
+        expect(body.includes('<label>Enter a URL to shorten: <input id="url" type="text"></label><br>')).toBeTrue;
+        expect(body.includes('<button name="submit" onclick="shortenUrl();">Shorten!</button>')).toBeTrue;
+        done();
+      });
+    });
+  });
+
+  describe('POST /', () => {
+    it('returns status code 200', (done) => {
+      request.post(base_url, { json: true, body: { url:'https://github.com/zky829/url-shortener' } }, (error, response, body) => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    it("returns shortened URL", (done) => {
+      request.post(base_url, { json: true, body: { url:'https://github.com/zky829/url-shortener' } }, (error, response, body) => {
+        expect(response.body.url.includes('https://turtl.es/')).toBeTrue;
+        done();
+      });
+    });
+  });
+});
